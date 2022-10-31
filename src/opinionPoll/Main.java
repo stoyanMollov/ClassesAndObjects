a4d3f0064e99b55dbd1624d93557e70c47f3906c
Stoyan Mollov <mollov9@gmail.com>
2022-10-31 04:30:43 +0200

    ClassesAndObjects

---
+++ src/opinionPoll/Main.java
@@ -0,0 +1,29 @@
+package opinionPoll;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Pearson> pearsonList = new ArrayList<>();
+
+        int n  = Integer.parseInt(scanner.nextLine());// бр. редове с лична информация
+
+        for (int row = 1; row <= n; row++) {
+            String data = scanner.nextLine(); // "Desi 24"
+            // "{име} {възраст}". split(" ") -> ["{име}", "{възраст}"]
+            String name = data.split(" ")[0];
+            int age = Integer.parseInt(data.split(" ")[1]);
+
+            if(age > 30){
+                // Създавам запис за човека -> съхраняване запис
+                Pearson pearson = new Pearson(name,age);
+                pearsonList.add(pearson);
+            }
+
+        }
+    }
+}
---
+++ src/opinionPoll/Pearson.java
@@ -0,0 +1,16 @@
+package opinionPoll;
+
+public class Pearson {
+    //1. трябва да опишем нашите полета -> характеристики -> име и възраст
+    private String name;
+    private int age;
+
+    //2. Кондтруктор -> създаваме обекти от ккласа
+    public Pearson(String name, int age){
+        // нове обект
+        this.name = name;
+        this.age = age;
+
+    }
+    //3. методи -> поведение.
+}
